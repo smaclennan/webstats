3a9e7da02362fa6527b8c9de9f4ca6fdb89f537a
Sean MacLennan <git@seanm.ca>
2012-04-02 19:24:20 -0400

    We don't need to separate out ftp.seanm.ca any more.

--- webstats.c
+++ webstats.c
@@ -27,7 +27,7 @@ static struct site {
 	{ "seanm.ca", 0xff0000 }, /* must be first! */
 	{ "rippers.ca", 0x0000ff },
 	{ "emacs", 0xffa500 },
-	{ "ftp.seanm.ca", 0x00ff00 },
+	/* { "ftp.seanm.ca", 0x00ff00 }, */
 };
 static int n_sites = sizeof(sites) / sizeof(struct site);
 
