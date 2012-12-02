a34acda89685b52945ea793d069edd56526dca42
Sean MacLennan <git@seanm.ca>
2012-12-02 16:07:07 -0500

    The check for robots.txt was very hit and miss. And for visits we want to see the robots.txt. So just remove the check.

--- is.c
+++ is.c
@@ -70,13 +70,6 @@ int isvisit(struct log *log, DB *ipdb)
 	if (strcmp(log->method, "GET"))
 		return 0;
 
-	if (strstr(log->url, "robot.txt")) {
-		if (verbose)
-			printf("Blacklist %s\n", log->ip);
-		db_put_data(ipdb, log->ip, &log->time, sizeof(log->time), 0);
-		return 0;
-	}
-
 	if (!ispage(log))
 		return 0;
 
